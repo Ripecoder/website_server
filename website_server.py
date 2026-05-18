89703eddcd7dc2f65dc3915c5f61d39eee032f1a
Ripecoder <n21816012@gmail.com>
2026-05-18 19:58:20 +0530

    Update website_server.py

--- website_server.py
+++ website_server.py
@@ -4,8 +4,19 @@ import time
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+
 app = Flask(__name__)
-CORS(app)
+
+CORS(
+    app,
+    resources={
+        r"/*": {
+            "origins": [
+                "https://verve-lemon-nine.vercel.app"
+            ]
+        }
+    }
+)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
